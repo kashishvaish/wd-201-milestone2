147612586e80a75ac0fd2d2aa67728ea5c902dd6
Kashish Vaish <kashishvaish.kv@gmail.com>
2022-08-03 14:28:16 +0530

    Initial Commit

---
+++ index.js
@@ -0,0 +1,48 @@
+const http = require("http");
+const fs = require("fs");
+
+let homeContent = "";
+let projectContent = "";
+let surveyContent = "";
+
+fs.readFile("home.html", function (err, home) {
+  if (err) {
+    throw err;
+  }
+  homeContent = home;
+});
+
+fs.readFile("project.html", function (err, project) {
+  if (err) {
+    throw err;
+  }
+  projectContent = project;
+});
+
+fs.readFile("registration.html", function (err, survey) {
+  if (err) {
+    throw err;
+  }
+  surveyContent = survey;
+});
+
+http
+  .createServer(function (request, response) {
+    let url = request.url;
+    response.writeHeader(200, { "Content-Type": "text/html" });
+    switch (url) {
+      case "/project":
+        response.write(projectContent);
+        response.end();
+        break;
+      case "/project/registration":
+        response.write(surveyContent);
+        response.end();
+        break;
+      default:
+        response.write(homeContent);
+        response.end();
+        break;
+    }
+  })
+  .listen(3000);
